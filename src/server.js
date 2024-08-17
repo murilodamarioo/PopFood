804e8f2afe31a78bc958ad08ed7a12d1b2d0d644
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-17 19:16:33 -0300

    feat: Implement AppErro class

--- src/server.js
+++ src/server.js
@@ -1,6 +1,30 @@
 const express = require('express')
+const cors = require('cors')
+const AppError = require('./utils/AppError')
 
 const app = express()
 
-const PORT = 3333
+// setting cors
+app.use(cors())
+
+// handle json data
+app.use(express.json())
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  console.log(error)
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error'
+  })
+})
+
+const PORT = process.env.SERVER_PORT || 3333
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
