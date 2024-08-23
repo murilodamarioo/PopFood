05012d62cfbe1c31750fbc0651504ccb8505f9e1
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-22 22:27:22 -0300

    feat apply middleware in foods routes

--- src/routes/foods.routes.js
+++ src/routes/foods.routes.js
@@ -2,8 +2,12 @@ const { Router } = require('express')
 
 const FoodsController = require('../controllers/FoodsController')
 
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+
 const foodsRoutes = Router()
 const foodsController = new FoodsController()
+
+foodsRoutes.use(ensureAuthenticated)
 
 foodsRoutes.get('/', foodsController.index)
 foodsRoutes.get('/:id', foodsController.show)
