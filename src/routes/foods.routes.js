02159580e1e63fca058b5105908c756c893fed25
Murilo Damario <murilo.gcdamario@gmail.com>
2024-08-25 18:15:59 -0300

    feat: implement provider for load image and delete image

--- src/routes/foods.routes.js
+++ src/routes/foods.routes.js
@@ -1,4 +1,6 @@
-const { Router } = require('express')
+const { Router, response } = require('express')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 
 const FoodsController = require('../controllers/FoodsController')
 
@@ -6,6 +8,7 @@ const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 const verifyUserAuthorization = require('../middlewares/verifyUserAuthorization')
 
 const foodsRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 const foodsController = new FoodsController()
 
 foodsRoutes.use(ensureAuthenticated)
@@ -13,6 +16,10 @@ foodsRoutes.use(ensureAuthenticated)
 foodsRoutes.get('/', foodsController.index)
 foodsRoutes.get('/:id', foodsController.show)
 foodsRoutes.post('/', verifyUserAuthorization(['admin']), foodsController.create)
+foodsRoutes.patch('/:id/image', verifyUserAuthorization(['admin']), upload.single('image'), (request, response) => {
+  console.log(request.file.filename)
+  return response.json()
+})
 foodsRoutes.delete('/:id', verifyUserAuthorization(['admin']), foodsController.delete)
 
 module.exports = foodsRoutes
