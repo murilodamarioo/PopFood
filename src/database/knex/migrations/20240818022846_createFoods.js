7e79be787aa0556df43bb61c5f12d5ee1c730a95
Murilo Damario <murilo.gcdamario@gmail.com>
2024-09-01 16:48:33 -0300

    refactor: add columns at foods table and ingredients table

--- src/database/knex/migrations/20240818022846_createFoods.js
+++ src/database/knex/migrations/20240818022846_createFoods.js
@@ -6,6 +6,9 @@ exports.up = knex => knex.schema.createTable('foods', table => {
   table.text('category')
   table.integer('price')
   table.text('description')
+  table.integer('user_id').references('id').inTable('users')
+  table.timestamp('created_at').default(knex.fn.now())
+  table.timestamp('updated_at').default(knex.fn.now())
 })
 
 exports.down = knex => knex.schema.dropTable('foods')
--- src/database/knex/migrations/20240818035150_createIngredients.js
+++ src/database/knex/migrations/20240818035150_createIngredients.js
@@ -2,6 +2,7 @@ exports.up = knex => knex.schema.createTable('ingredients', table => {
   table.increments('id')
   table.text('title')
   table.integer('food_id').references('id').inTable('foods').onDelete('CASCADE')
+  table.integer('user_id').references('id').inTable('users').onDelete('CASCADE')
 })
 
 exports.down = knex => knex.schema.dropTable('ingredients')
