9492d4ca57244335907a4c8482e56de35e54a975
murilodamarioo <murilo.gcdamario@gmail.com>
2024-09-07 18:18:28 -0300

    feat: Implement Swagger documentation route into index file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 
+const swaggerRoutes = require('./documentation.routes')
 const sessionsRoutes = require('./sessions.routes')
 const usersRoutes = require('./users.routes')
 const foodsRoutes = require('./foods.routes')
 
 const routes = Router()
 
+routes.use('/api-docs', swaggerRoutes)
 routes.use('/sessions', sessionsRoutes)
 routes.use('/users', usersRoutes)
 routes.use('/foods', foodsRoutes)
